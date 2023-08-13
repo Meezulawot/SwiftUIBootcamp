07a4de8e4dbde7fb9ae2b56619b18e6c37579fc5
Meezu Lawot <meezu.lawot@swifttech.com.np>
2023-08-13 10:25:50 +0545

    Initial Commit

---
+++ SwiftUiBootcamp/SwiftUiBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBootcampApp.swift
+//  SwiftUiBootcamp
+//
+//  Created by Meezu Lawot on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
